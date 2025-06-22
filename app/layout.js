6057c848370efd5b35a01fc39c3cc275cdcf4b72
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-06-22 15:06:43 +0630

    add starbase page and electra page

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Header />
+        <h1>Introducing Nomadtrac</h1>
         {children}
         <Footer />
       </body>
--- app/page.js
+++ app/page.js
@@ -1,40 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
-import Hero from "@/public/hero.jpg";
-import Electra from "@/public/electra.jpg";
-import Shuttle from "@/public/shuttle.jpg";
-import Starbase from "@/public/starbase.jpg";
+import HeroImg from "@/public/hero.jpg";
+import ElectraImg from "@/public/electra.jpg";
+import ShuttleImg from "@/public/shuttle.jpg";
+import StarbaseImg from "@/public/starbase.jpg";
 
 export default function Home() {
   return (
     <>
       <div>
-        <h1>Introducing Nomadtrac</h1>
         <h2>The First Self-Driving Camper</h2>
         <h3>Because the Planet is Your Home</h3>
       </div>
 
       <div className="heroDiv">
-        <Image priority src={Hero} fill alt="camper under the stars" />
+        <Image priority src={HeroImg} fill alt="camper under the stars" />
       </div>
 
       <h2>A Model For Every Lifestyle</h2>
 
       <div className="thumbnailDiv">
         <Link href="/shuttle" className="navLink">
-          <Image src={Shuttle} fill alt="shuttle" />
+          <Image src={ShuttleImg} fill alt="shuttle" />
+          <br />
           <h3>Shuttle</h3>
         </Link>
       </div>
 
       <div className="thumbnailDiv">
-        <Image src={Electra} fill alt="electra" />
-        <h3>Electra</h3>
+        <Link href="/electra" className="navLink">
+          <Image src={ElectraImg} fill alt="electra" />
+          <br />
+          <h3>Electra</h3>
+        </Link>
       </div>
 
       <div className="thumbnailDiv">
-        <Image src={Starbase} fill alt="starbase" />
-        <h3>Starbase</h3>
+        <Link href="/starbase" className="navLink">
+          <Image src={StarbaseImg} fill alt="starbase" />
+          <br />
+          <h3>Starbase</h3>
+        </Link>
       </div>
     </>
   );
